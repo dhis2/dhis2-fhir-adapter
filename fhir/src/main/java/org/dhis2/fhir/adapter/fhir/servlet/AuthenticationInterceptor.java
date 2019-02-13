3c0ae7df427e8befe8c0e0ab6a807d0bb07f2baa
Volker Schmidt <volker@volsch.eu>
2019-02-13 22:41:22 +0100

    Added servlets for FHIR interfaces.

---
+++ fhir/src/main/java/org/dhis2/fhir/adapter/fhir/servlet/AuthenticationInterceptor.java
@@ -0,0 +1,70 @@
+package org.dhis2.fhir.adapter.fhir.servlet;
+
+/*
+ * Copyright (c) 2004-2019, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import ca.uhn.fhir.rest.api.server.RequestDetails;
+import ca.uhn.fhir.rest.server.exceptions.AuthenticationException;
+import ca.uhn.fhir.rest.server.interceptor.InterceptorAdapter;
+import org.dhis2.fhir.adapter.auth.Authorization;
+import org.dhis2.fhir.adapter.auth.AuthorizationContext;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Nonnull;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * HAPI FHIR Server authentication interceptor. The specified authorization is passed
+ * as authorization to be used by DHIS2.
+ *
+ * @author volsch
+ */
+@Component
+@Order( 1 )
+public class AuthenticationInterceptor extends InterceptorAdapter
+{
+    protected static final String AUTHORIZATION_HEADER_NAME = "Authorization";
+
+    private final AuthorizationContext authorizationContext;
+
+    public AuthenticationInterceptor( @Nonnull AuthorizationContext authorizationContext )
+    {
+        this.authorizationContext = authorizationContext;
+    }
+
+    @Override
+    public boolean incomingRequestPostProcessed( RequestDetails theRequestDetails, HttpServletRequest theRequest, HttpServletResponse theResponse ) throws AuthenticationException
+    {
+        final String authorization = theRequest.getHeader( AUTHORIZATION_HEADER_NAME );
+        final Authorization requestedAuthorization = new Authorization( authorization );
+        authorizationContext.setAuthorization( requestedAuthorization );
+        return true;
+    }
+}
---
+++ fhir/src/main/java/org/dhis2/fhir/adapter/fhir/servlet/FhirRestfulServer.java
@@ -0,0 +1,84 @@
+package org.dhis2.fhir.adapter.fhir.servlet;
+
+/*
+ * Copyright (c) 2004-2019, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.server.IResourceProvider;
+import ca.uhn.fhir.rest.server.RestfulServer;
+import ca.uhn.fhir.rest.server.interceptor.IServerInterceptor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.core.annotation.AnnotationAwareOrderComparator;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Abstract FHIR restful server that can be used with different FHIR versions.
+ *
+ * @author volsch
+ */
+public class FhirRestfulServer extends RestfulServer
+{
+    private static final long serialVersionUID = -8746071131827306156L;
+
+    private final List<IResourceProvider> resourceProviders;
+
+    private final List<IServerInterceptor> interceptors;
+
+    public FhirRestfulServer( @Nonnull FhirContext fhirContext,
+        @Nonnull ObjectProvider<List<IResourceProvider>> resourceProviders,
+        @Nonnull ObjectProvider<List<IServerInterceptor>> interceptors )
+    {
+        super( fhirContext );
+        this.resourceProviders = resourceProviders.getIfAvailable( Collections::emptyList );
+        this.interceptors = sortByOrderAnnotation( interceptors.getIfAvailable() );
+    }
+
+    @Override
+    protected void initialize()
+    {
+        setResourceProviders( resourceProviders );
+        setInterceptors( interceptors );
+    }
+
+    @Nullable
+    private static <T> List<T> sortByOrderAnnotation( @Nullable List<T> list )
+    {
+        if ( list == null )
+        {
+            return null;
+        }
+        final List<T> copiedList = new ArrayList<>( list );
+        AnnotationAwareOrderComparator.sort( copiedList );
+        return copiedList;
+    }
+}
